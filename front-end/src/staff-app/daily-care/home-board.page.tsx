02fb32861cb9626efbd13060313d4568352f4b72
Raj Gohel <rajgohel0001@gmail.com>
2022-06-11 18:19:59 +0530

    Added searching and sorting functionality

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -9,18 +9,65 @@ import { Person } from "shared/models/person"
 import { useApi } from "shared/hooks/use-api"
 import { StudentListTile } from "staff-app/components/student-list-tile/student-list-tile.component"
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
+import { Input, Switch } from "@material-ui/core"
 
 export const HomeBoardPage: React.FC = () => {
   const [isRollMode, setIsRollMode] = useState(false)
+  const [sortMode, setSortMode] = useState(false)
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
+  const [studentsData, setStudentsData] = useState(data?.students || []);
 
   useEffect(() => {
-    void getStudents()
+    void getStudents();
   }, [getStudents])
 
-  const onToolbarAction = (action: ToolbarAction) => {
+  useEffect(() => {
+    if (data?.students) {
+      setStudentsData(data?.students);
+    }
+  }, [data?.students])
+
+  const onToolbarAction = (action: ToolbarAction, value?: string) => {
     if (action === "roll") {
       setIsRollMode(true)
+    }
+    if (action === "sort" && data?.students) {
+      setSortMode((prevState) => !prevState);
+      // Sort by first name
+      if (value !== "lastName") {
+        let firstNameSortedData = [];
+        // Desc order
+        if (sortMode) firstNameSortedData = data.students.sort((a, b) => b.first_name.localeCompare(a.first_name));
+        // Asc order
+        else firstNameSortedData = data.students.sort((a, b) => a.first_name.localeCompare(b.first_name));
+        setStudentsData(firstNameSortedData);
+      }
+      // Sort by last name
+      else {
+        let lastNameSortedData = [];
+        if (sortMode) lastNameSortedData = data.students.sort((a, b) => b.last_name.localeCompare(a.last_name));
+        else lastNameSortedData = data.students.sort((a, b) => a.last_name.localeCompare(b.last_name));
+        setStudentsData(lastNameSortedData);
+      }
+    }
+  }
+
+  const handleInputChange = (e: any) => {
+    let keyword: string = e.target.value.toLowerCase();
+    if (data?.students && !keyword.trim().length) setStudentsData(data.students);
+    else if (data?.students) {
+      let filteredData = data.students.filter((ele) => ele.first_name.toLowerCase().indexOf(keyword) !== -1 || ele.last_name.toLowerCase().indexOf(keyword) !== -1);
+      setStudentsData(filteredData);
+    }
+  };
+
+  const handleToggleChange = () => {
+    if (data?.students) {
+      let sortedData = [];
+      if (sortMode) sortedData = data.students.sort((a, b) => (b.first_name + b.last_name).localeCompare(a.first_name + a.last_name));
+      else sortedData = data.students.sort((a, b) => (a.first_name + a.last_name).localeCompare(b.first_name + b.last_name));
+      setSortMode((prevState) => !prevState);
+      setStudentsData(sortedData);
     }
   }
 
@@ -33,7 +80,11 @@ export const HomeBoardPage: React.FC = () => {
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction}
+          onInputChange={handleInputChange}
+          onToggleChange={handleToggleChange}
+          sortOrder={sortMode}
+        />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -41,9 +92,9 @@ export const HomeBoardPage: React.FC = () => {
           </CenteredContainer>
         )}
 
-        {loadState === "loaded" && data?.students && (
+        {loadState === "loaded" && studentsData && (
           <>
-            {data.students.map((s) => (
+            {studentsData.map((s) => (
               <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
             ))}
           </>
@@ -62,14 +113,19 @@ export const HomeBoardPage: React.FC = () => {
 
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
-  onItemClick: (action: ToolbarAction, value?: string) => void
+  onItemClick: (action: ToolbarAction, value?: string) => void,
+  onInputChange: (e: any) => void,
+  onToggleChange: (e: any) => void,
+  sortOrder: boolean
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, onInputChange, onToggleChange, sortOrder } = props
   return (
     <S.ToolbarContainer>
-      <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <Switch name="sortVal" checked={sortOrder} size="small" onChange={onToggleChange} />
+      <S.Button onClick={() => onItemClick("sort")}>First Name</S.Button>
+      <S.Button onClick={() => onItemClick("sort", "lastName")}>Last Name</S.Button>
+      <Input placeholder="Search..." style={{ color: "#fff" }} onChange={onInputChange} />
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
